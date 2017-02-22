5f5d316468738a3457946170f1f67f1e309c01f6
dening88 <dening88@gmail.com>
2017-02-23 03:26:10 +0700

    123123

---
+++ src/main/java/firstTest/Main.java
@@ -0,0 +1,9 @@
+package firstTest;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!!!");
+    }
+}
---
+++ src/main/java/firstTest/Test2.java
@@ -0,0 +1,9 @@
+package firstTest;
+
+public class Test2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My test with github.com!!!");
+    }
+}
